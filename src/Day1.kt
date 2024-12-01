c09f3d5267ba72942438ec34da307e1dedcd8b8b
Andreas Wassmer <andreas.wassmer@me.com>
2024-12-01 12:03:31 +0100

    Solved both parts.

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,6 +1,6 @@
 fun part1(input: List<String>): Long {
-    var left = mutableListOf<Int>()
-    var right = mutableListOf<Int>()
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
 
     for (line in input) {
         val tokens = line.split("\\s+".toRegex())
@@ -18,11 +18,35 @@ fun part1(input: List<String>): Long {
     return sum
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
+fun part2(input: List<String>): Long {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    val pairs = hashMapOf<Int, Int>()
+
+    for (line in input) {
+        val tokens = line.split("\\s+".toRegex())
+        left.add(tokens[0].toInt())
+        right.add(tokens[1].toInt())
+        pairs[tokens[0].toInt()] = 0
+    }
+
+    for (number_1 in left) {
+        for (number_2 in right) {
+            if (number_1 == number_2) {
+                pairs[number_1] = pairs[number_1]!! + 1
+            }
+        }
+    }
+
+    println(pairs)
+    var sum: Long = 0
+    for (keys in pairs.keys) {
+        sum += keys * pairs[keys]!!
+    }
+    return sum
 }
 
 fun main() {
-    val input = readInput("day1_test")
-    check(part1(input) == 11L)
+    val input = readInput("day1_quiz")
+    print("Solution: ${part2(input)}")
 }
