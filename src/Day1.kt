553bb0fde1d0c162f54891428eddf3894cd78cd3
Andreas Wassmer <andreas.wassmer@me.com>
2024-12-01 19:01:54 +0100

    Less complex solution for part 2.

--- src/Day1.kt
+++ src/Day1.kt
@@ -19,29 +19,29 @@ fun part1(input: List<String>): Long {
 }
 
 fun part2(input: List<String>): Long {
-    val left = mutableListOf<Int>()
-    val right = mutableListOf<Int>()
+    val left = hashMapOf<Int, Int>()
+    val right = hashMapOf<Int, Int>()
     val pairs = hashMapOf<Int, Int>()
 
     for (line in input) {
         val tokens = line.split("\\s+".toRegex())
-        left.add(tokens[0].toInt())
-        right.add(tokens[1].toInt())
-        pairs[tokens[0].toInt()] = 0
-    }
-
-    for (number_1 in left) {
-        for (number_2 in right) {
-            if (number_1 == number_2) {
-                pairs[number_1] = pairs[number_1]!! + 1
-            }
+        if (!left.containsKey(tokens[0].toInt())) {
+            left[tokens[0].toInt()] = 1
+        } else {
+            left[tokens[0].toInt()] = left[tokens[0].toInt()]!! + 1
+        }
+        if (!right.containsKey(tokens[1].toInt())) {
+            right[tokens[1].toInt()] = 1
+        } else {
+            right[tokens[1].toInt()] = right[tokens[1].toInt()]!! + 1
         }
     }
 
-    println(pairs)
     var sum: Long = 0
-    for (keys in pairs.keys) {
-        sum += keys * pairs[keys]!!
+    for (value in left.keys) {
+        if (right.containsKey(value)) {
+            sum += left[value]!! * (value * right[value]!!)
+        }
     }
     return sum
 }
