1ebb6e524d4fab94cb8f1b6ad30f50262dc61740
Andreas Wassmer <andreas.wassmer@me.com>
2024-12-01 10:52:32 +0100

    first attempt withg test

--- src/Day1.kt
+++ src/Day1.kt
@@ -1,5 +1,21 @@
-fun part1(input: List<String>): Int {
-    return input.size
+fun part1(input: List<String>): Long {
+    var left = mutableListOf<Int>()
+    var right = mutableListOf<Int>()
+
+    for (line in input) {
+        val tokens = line.split("\\s+".toRegex())
+        left.add(tokens[0].toInt())
+        right.add(tokens[1].toInt())
+    }
+    left.sort()
+    right.sort()
+
+    var sum: Long = 0
+    for (i in left.indices) {
+        sum += Math.abs(left[i].toLong() - right[i])
+    }
+
+    return sum
 }
 
 fun part2(input: List<String>): Int {
@@ -7,6 +23,6 @@ fun part2(input: List<String>): Int {
 }
 
 fun main() {
-    val input = readInput("input/day1_test")
-    check(part1(input) == 2)
+    val input = readInput("day1_test")
+    check(part1(input) == 11L)
 }
