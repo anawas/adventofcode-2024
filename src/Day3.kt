8ac40e7249d2e47c6e5f5d4ced97d8748ad96bde
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2024-12-03 13:47:40 +0100

    Solved day 3, part 1

--- src/Day3.kt
+++ src/Day3.kt
@@ -2,23 +2,26 @@ class Day3 {
     fun part1(input: List<String>): Int {
         val operationRegex: Regex = """mul\(\d+,\d+\)""".toRegex()
         val argumentRegex: Regex = """\d+,\d+""".toRegex()
+        var sum: Int = 0
         for (line in input) {
             for (operation in operationRegex.findAll(line)) {
                 val arguments = argumentRegex.findAll(operation.value)
-                for (args in arguments)
-                    println(args.value)
+                for (matchResult in arguments) {
+                    val args = matchResult.value.split(",")
+                    sum += args[0].toInt() * args[1].toInt()
+                }
             }
-
         }
-        return input.size
+        return sum
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 }
+
 fun main() {
     val quiz = Day3()
-    val input = readInput("day3_test")
-    check(quiz.part1(input) == 1)
+    val input = readInput("day3_puzzle")
+    println("Solution: ${quiz.part1(input)}")
 }
