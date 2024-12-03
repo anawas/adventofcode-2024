1b56a719a8aacead7a35ae8b345d95bdfc6548e4
Andreas Wassmer <andreas.wassmer@me.com>
2024-12-03 06:49:21 +0100

    Added template.

---
+++ src/Day3.kt
@@ -0,0 +1,23 @@
+import java.nio.file.NoSuchFileException
+import kotlin.system.exitProcess
+
+class Day3 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
+
+fun main() {
+    val quiz = Day3()
+    try {
+        val input = readInput("day2_puzzle")
+        check(quiz.part1(input) == 6)
+    } catch (e: NoSuchFileException) {
+        println("ERROR -- Cannot read input")
+        exitProcess(1)
+    }
+}
