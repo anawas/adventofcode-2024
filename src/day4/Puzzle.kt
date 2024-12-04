458cb88d5aedb1e1298029b46e58244b5451ae77
Andreas Wassmer <andreas.wassmer@me.com>
2024-12-04 21:29:31 +0100

    Code clean up

--- src/day4/Puzzle.kt
+++ src/day4/Puzzle.kt
@@ -4,7 +4,6 @@ import utilities.readInput
 
 class Puzzle {
     private fun getSearchPatterns(lineLength: Int): HashMap<String, List<Int>> {
-        val patternLength = "XMAS".length
         val patterns: HashMap<String, List<Int>> = hashMapOf()
         patterns["leftright"]  = listOf(0, 1, 2, 3)
         patterns["rightleft"]  = listOf(3, 2, 1, 0)
@@ -12,7 +11,7 @@ class Puzzle {
         patterns["bottomtop"]  = listOf(3 * lineLength, 2 * lineLength, 1 * lineLength, 0)
         patterns["topleftbottomright"] = listOf(0, 1 * lineLength + 1, 2 * lineLength + 2, 3 * lineLength + 3)
         patterns["bottomlefttopright"] = listOf(3*lineLength, 2 * lineLength + 1, 1 * lineLength + 2, 3)
-        patterns["toprightbottomleft"] = listOf(3, lineLength +2, 2 * lineLength + 1, 3 * lineLength + 0)
+        patterns["toprightbottomleft"] = listOf(3, lineLength + 2, 2 * lineLength + 1, 3 * lineLength + 0)
         patterns["bottomrighttopleft"] = listOf(3*lineLength+3, 2 * lineLength + 2, 1 * lineLength + 1, 0)
         return patterns
     }
@@ -29,166 +28,78 @@ class Puzzle {
 
         val charArray: CharArray = puzzle.toCharArray()
         val searchPattern = getSearchPatterns(lineLength)
-
-        var pattern = searchPattern["leftright"]!!
         var totalSum = 0
 
+        // First we search horizontally
+        var patterns = listOf(searchPattern["leftright"]!!, searchPattern["rightleft"]!!)
         var counter = 0
         for (i in 0 until lineLength-3) {
             for (j in 0 until numOfLines) {
                 val letters = CharArray(4)
                 val index = i + j * lineLength
-
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
+                for (pattern in patterns) {
+                    for (k in pattern.indices) {
+                        letters[k] = charArray[index + pattern[k]]
+                    }
+                    val word = String(letters)
+                    if (word == "XMAS") {
+                        ++counter
+                    }
                 }
             }
         }
 
         totalSum += counter
-        println("Found ${counter}x in leftright ")
+        println("Found ${counter}x in horizontal search")
 
-        pattern = searchPattern["rightleft"]!!
-        counter = 0
-        for (i in 0 until lineLength-3) {
-            for (j in 0 until numOfLines) {
-                val letters = CharArray(4)
-                val index = i + j * lineLength
-
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
-                }
-            }
-        }
-        totalSum += counter
-        println("Found ${counter}x in rightleft ")
-
-        pattern = searchPattern["topbottom"]!!
+        // Next search vertically
+        patterns = listOf(searchPattern["topbottom"]!!, searchPattern["bottomtop"]!!)
         counter = 0
         for (i in 0 until lineLength) {
             for (j in 0 until numOfLines-3) {
                 val letters = CharArray(4)
                 val index = i + j * lineLength
 
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
+                for (pattern in patterns) {
+                    for (k in pattern.indices) {
+                        letters[k] = charArray[index + pattern[k]]
+                    }
+                    val word = String(letters)
+                    if (word == "XMAS") {
+                        ++counter
+                    }
                 }
             }
         }
         totalSum += counter
-        println("Found ${counter}x in topbottom")
+        println("Found ${counter}x in vertical search")
 
-        pattern = searchPattern["bottomtop"]!!
-        counter = 0
-        for (i in 0 until lineLength) {
-            for (j in 0 until numOfLines-3) {
-                val letters = CharArray(4)
-                val index = i + j * lineLength
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
-                }
-            }
-        }
+        // now we search diagonally, There are 4 diagonals!
+        patterns = listOf(
+            searchPattern["topleftbottomright"]!!,
+            searchPattern["bottomlefttopright"]!!,
+            searchPattern["toprightbottomleft"]!!,
+            searchPattern["bottomrighttopleft"]!!)
 
-        totalSum += counter
-        println("Found ${counter}x in bottomtop")
-
-        pattern = searchPattern["topleftbottomright"]!!
         counter = 0
         for (i in 0 until lineLength-3) {
             for (j in 0 until numOfLines-3) {
                 val letters = CharArray(4)
                 val index = i + j * lineLength
 
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
-                }
-            }
-        }
-
-        totalSum += counter
-        println("Found ${counter}x in topleftbottomright")
-
-        pattern = searchPattern["bottomlefttopright"]!!
-        counter = 0
-        for (i in 0 until lineLength-3) {
-            for (j in 0 until numOfLines-3) {
-                val letters = CharArray(4)
-                val index = i + j * lineLength
-
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
+                for (pattern in patterns) {
+                    for (k in pattern.indices) {
+                        letters[k] = charArray[index + pattern[k]]
+                    }
+                    val word = String(letters)
+                    if (word == "XMAS") {
+                        ++counter
+                    }
                 }
             }
         }
-
         totalSum += counter
-        println("Found ${counter}x in bottomlefttopright")
-
-        pattern = searchPattern["toprightbottomleft"]!!
-        counter = 0
-        for (i in 0 until lineLength- 3) {
-            for (j in 0 until numOfLines-3) {
-                val letters = CharArray(4)
-                val index = i + j * lineLength
-
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
-                }
-            }
-        }
-
-        totalSum += counter
-        println("Found ${counter}x in toprightbottomleft")
-
-        pattern = searchPattern["bottomrighttopleft"]!!
-        counter = 0
-        for (i in 0 until lineLength-3) {
-            for (j in 0 until numOfLines-3) {
-                val letters = CharArray(4)
-                val index = i + j * lineLength
-
-                for (k in pattern.indices) {
-                    letters[k] = charArray[index + pattern[k]]
-                }
-                val word = String(letters)
-                if (word == "XMAS") {
-                    ++counter
-                }
-            }
-        }
-
-        totalSum += counter
-        println("Found ${counter}x in bottomrightttopleft")
-
+        println("Found ${counter}x on diagonals")
         return totalSum
     }
 
