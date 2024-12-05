4f0e4dd2e7db1ba374c24b7ef93e7d4c5440bf8e
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2024-12-05 14:41:29 +0100

    Parsing input for day 5

--- src/day5/Puzzle.kt
+++ src/day5/Puzzle.kt
@@ -3,16 +3,44 @@ package day5
 import utilities.readInput
 
 class Puzzle {
+    data class PageOrderingRule(val firstPage: Int, val secondPage:Int)
+
+    private fun getPageOrderingRules(input: List<String>): List<PageOrderingRule> {
+        val orderingRules:MutableList<PageOrderingRule> = mutableListOf()
+        for (line in input) {
+            if (line.isBlank()) break
+            val values = line.split("|")
+            orderingRules.add(PageOrderingRule(values[0].toInt(), values[1].toInt()))
+        }
+        return orderingRules
+    }
+
+    private fun getProductionRules(input: List<String>, startIndex: Int): List<List<Int>> {
+        val pageProductionList:MutableList<List<Int>> = mutableListOf()
+        for (i in startIndex until input.size) {
+            if (input[i].isBlank()) continue
+            val tokens = input[i].split(",")
+            val production = tokens.stream().map(Integer::parseInt).toList()
+            pageProductionList.add(production)
+        }
+        return pageProductionList
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val orderingRules: List<PageOrderingRule> = getPageOrderingRules(input)
+        val productionLists: List<List<Int>> = getProductionRules(input, orderingRules.size)
+        println("Found ${orderingRules.size} page ordering rules")
+        println("and ${productionLists.size} production rules")
+        return orderingRules.size
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 }
+
 fun main() {
     val quiz = Puzzle()
-    val input = readInput("puzzle_test")
-    check(quiz.part1(input) == 3)
+    val input = readInput("day5_test")
+    check(quiz.part1(input) == 21)
 }
